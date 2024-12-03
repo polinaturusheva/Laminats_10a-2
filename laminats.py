0a79e07e7a78de1c61f924ba4a7c92eed8c74907
Polina Turusheva <polina.turuseva@gmail.com>
2024-12-03 10:00:14 +0200

    Update laminats.py

--- laminats.py
+++ laminats.py
@@ -26,5 +26,26 @@ def aprekinat_laminatu(plaksnes_garums, plaksnes_platums, istabas_garums, istaba
 
     return kopejas_plaksnes
 
-# pārbaude
+
 print(aprekinat_laminatu(1, .5, 3, 2))
+
+import math
+
+room_length = float(input("Istabas garums (m): "))
+room_width = float(input("Istabas platums (m): "))
+package_price = float(input("Lamināta pakas cena (EUR): "))
+package_area = float(input("Lamināta pakas sedzošā platība (m²): "))
+plank_length = float(input("Plāksnes garums (m): "))
+plank_width = float(input("Plāksnes platums (m): "))
+
+longest_wall = max(room_length, room_width)
+plank_area = plank_length * plank_width
+planks_per_package = package_area / plank_area
+room_area = room_length * room_width
+required_planks = room_area / plank_area
+required_packages = math.ceil(required_planks / planks_per_package)
+
+print(f"Istabas garākā siena: {longest_wall:.2f} m")
+print(f"Plāksnes vienā pakā: {math.floor(planks_per_package)} gab.")
+print(f"Nepieciešamais plākšņu skaits: {math.ceil(required_planks)} gab.")
+print(f"Nepieciešamais paku skaits: {required_packages} gab.")
